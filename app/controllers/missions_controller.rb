1f2ecd26ab6955f9036482d85e0bafefda1340bb
henry blackburn <henryjamesblackburn@hotmail.co.uk>
2022-02-22 14:35:56 +0100

    finised writing methods in mission controller and added simple for to mission edit view

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,14 +1,15 @@
 class MissionsController < ApplicationController
+  before_action :find_mission, only: %i[edit show edit]
   def new
     @mission = Mission.new
-    @hero = Hero.find(params[id])
+    @hero = Hero.find(params[:hero_id])
   end
 
   def create
     @hero = Hero.find(params[:hero_id])
     @mission = Mission.new(mission_params)
-    @mission.user_id = current_user
-    @mission.hero_id = @hero
+    @mission.user_id = current_user.id
+    @mission.hero_id = @hero.id
     @mission.save
     if @mission.save
       redirect_to mission_path(@mission)
@@ -18,26 +19,23 @@ class MissionsController < ApplicationController
   end
 
   def edit
-    @mission = Mission.find(params[:mission_id])
   end
 
   def update
-    @mission = Mission.find(params[:mission_id])
     @mission = Mission.update(mission_params)
-    if @mission.save
-      redirect_to mission_path(@mission)
-    else
-      render :edit
-    end
+    redirect_to mission_path(@mission)
   end
 
   def show
-    @mission = Mission.find(params[:mission_id])
   end
 
   private
 
   def mission_params
     params.require(:mission).permit(:start_date, :end_date, :location, :title, :content, :status)
+  end
+
+  def find_mission
+    @mission = Mission.find(params[:id])
   end
 end
