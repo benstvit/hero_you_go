d5f6403ee298bd5275d0dc9879c1ea52d4342fe9
henry blackburn <henryjamesblackburn@hotmail.co.uk>
2022-02-22 20:36:13 +0100

    added migration file to add default value of pending to status column in missions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_21_161202) do
+ActiveRecord::Schema.define(version: 2022_02_22_193145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2022_02_21_161202) do
   create_table "missions", force: :cascade do |t|
     t.date "start_date"
     t.date "end_date"
-    t.string "status"
+    t.string "status", default: "pending"
     t.string "location"
     t.text "content"
     t.string "title"
