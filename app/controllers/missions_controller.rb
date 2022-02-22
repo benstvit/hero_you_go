c3fed7704c42a556fa692a4e51605bc5f35f7a9d
henry blackburn <henryjamesblackburn@hotmail.co.uk>
2022-02-22 10:56:02 +0100

    added strong params to mission controller

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,10 +1,17 @@
 class MissionsController < ApplicationController
   def new
+    @mission = Mission.new
   end
 
   def edit
   end
 
   def show
+  end
+
+  private
+
+  def mission_params
+
   end
 end
