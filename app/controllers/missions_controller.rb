2314b1ec9b2c508dadd86abd0634fa0c1fa846fb
henry blackburn <henryjamesblackburn@hotmail.co.uk>
2022-02-22 12:25:25 +0100

    Added edit and show to controller and simple form to new mission page

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,12 +1,38 @@
 class MissionsController < ApplicationController
   def new
     @mission = Mission.new
+    @hero = Hero.find(params[id])
+  end
+
+  def create
+    @hero = Hero.find(params[:hero_id])
+    @mission = Mission.new(mission_params)
+    @mission.user_id = current_user
+    @mission.hero_id = @hero
+    @mission.save
+    if @mission.save
+      redirect_to mission_path(@mission)
+    else
+      render :new
+    end
   end
 
   def edit
+    @mission = Mission.find(params[:mission_id])
+  end
+
+  def update
+    @mission = Mission.find(params[:mission_id])
+    @mission = Mission.update(mission_params)
+    if @mission.save
+      redirect_to mission_path(@mission)
+    else
+      render :edit
+    end
   end
 
   def show
+    @mission = Mission.find(params[:mission_id])
   end
 
   private
