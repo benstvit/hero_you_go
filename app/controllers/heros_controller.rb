f06c16c865346750b818d092eb3b6e453643ea38
Valentine Bodart <valentine.bodart@gmail.com>
2022-02-22 16:54:52 +0100

    ok

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -14,9 +14,19 @@ class HerosController < ApplicationController
   end
 
   def create
-    @hero = Hero.new(hero_params)
-
+    power_ids = params[:hero][:powers]
+    @hero = Hero.new(
+      name: params[:hero][:name],
+      location: params[:hero][:location],
+      price: params[:hero][:price]
+    )
+    @hero.user = current_user
     if @hero.save
+      power_ids.each do |power_id|
+        power = Power.find(power_id)
+        # HeroPower.create(hero: @hero, power: power)
+        @hero.powers << power
+      end
       redirect_to hero_path(@hero)
     else
       render :new
@@ -27,6 +37,6 @@ class HerosController < ApplicationController
   end
 
   def hero_params
-    params.require(:hero).permit(:name, :location, :price, :users_id)
+    params.require(:hero).permit(:name, :location, :price, powers: [])
   end
 end
