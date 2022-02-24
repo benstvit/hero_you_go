42a688657259057732d5e12e8775d44015c06c3a
Benjamin Saint Viteux <ben.saintviteux@gmail.com>
2022-02-24 16:19:26 +0100

    map working + top 5

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @top_heros = Hero.where(rating: 5)
+    @top_reviews = Review.where(rating: 5)
     @heros = Hero.all
   end
 
